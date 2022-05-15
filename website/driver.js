46b103e3ee4fbb5f7f604b54fac0fd9bd7f0c239
A Samuel Pottinger <sam.pottinger@clarafoods.com>
2022-05-15 11:07:48 -0600

    Update code styling on driver.js

--- website/driver.js
+++ website/driver.js
@@ -1,22 +1,58 @@
+/**
+ * Main entry point and logic for interfacing with the visualization webpage.
+ *
+ * @author A Samuel Pottinger
+ * @license MIT
+ */
+
 let currentPresenter = null;
 let cachedDataset = null;
 let filtersOpen = false;
 let lastClientWidth = -1;
 
 
-function addCheckboxListener(target) {
+/**
+ * Add a listener to an input that performs a "hard" redraw.
+ *
+ * Add a listener to a input that performs a "hard" redraw when the value of
+ * that input changes. A "hard" redraw will clear all existing elements and
+ * make new ones, required if the structure of the underlying data
+ * dramatically changes.
+ *
+ * @param target The HTMLElement to which the event listener should be added.
+ */
+function addHardRedrawListener(target) {
   target.addEventListener("change", () => {
     hardRedraw();
   });
 }
 
 
-function addCheckboxListenerById(targetId) {
+/**
+ * Add a listener to an input that performs a "hard" redraw.
+ *
+ * Add a listener to an input that performs a "hard" redraw when the value of
+ * that input changes. A "hard" redraw will clear all existing elements and
+ * make new ones, required if the structure of the underlying data
+ * dramatically changes.
+ *
+ * @param targetId The ID (without #) of the element to which the event
+ *   listener should be added.
+ */
+function addHardRedrawListenerById(targetId) {
   const target = document.getElementById(targetId);
-  addCheckboxListener(target);
+  addHardRedrawListener(target);
 }
 
 
+/**
+ * Add a listener to an input that performs a "soft" redraw.
+ *
+ * Add a listener to a input that performs a "soft" redraw when the value of
+ * that input changes. A "soft" redraw will update existing elements.
+ *
+ * @param target The HTMLElement to which the event listener should be added.
+ */
 function addRedrawListener(target) {
   target.addEventListener("change", () => {
     updateViz();
@@ -24,6 +60,15 @@ function addRedrawListener(target) {
 }
 
 
+/**
+ * Add a listener to an input that performs a "soft" redraw.
+ *
+ * Add a listener to an input that performs a "soft" redraw when the value of
+ * that input changes. A "soft" redraw will update existing elements.
+ *
+ * @param targetId The ID (without #) of the element to which the event
+ *   listener should be added.
+ */
 function onScroll(event) {
   const scolledDown = window.pageYOffset > 300;
   const glyphHolder = d3.select("#glyphLegendHolder");
@@ -31,6 +76,12 @@ function onScroll(event) {
 }
 
 
+/**
+ * Perform the initial drawing of the visualization.
+ *
+ * Perform the initial drawing of the visualization, causing an intro fade in
+ * animation to play, hiding the loading indicator.
+ */
 function initalLoadViz() {
   const fadeInViz = () => {
     d3.select("#loadingIndicator")
@@ -46,6 +97,11 @@ function initalLoadViz() {
 }
 
 
+/**
+ * Toggle if the filters panel is shown to the user.
+ *
+ * @param event The event causing this filter panel visibility change.
+ */
 function toggleFiltersPanel(event) {
   const filtersPanel = document.getElementById("filtersPanel");
   if (filtersOpen) {
@@ -64,6 +120,12 @@ function toggleFiltersPanel(event) {
 }
 
 
+/**
+ * Get the max value to show to the user on the percent difference gap axis.
+ *
+ * @returns The max percent gap value to show to the user. Values over this
+ *   will be off screen.
+ */
 function getGapMinMax() {
   const zoomingAxisCheck = document.getElementById("zoomingAxisCheck");
   const isZoomingAxis = zoomingAxisCheck.checked;
@@ -73,6 +135,11 @@ function getGapMinMax() {
 }
 
 
+/**
+ * Determine if the user has requested colorblind mode.
+ *
+ * @returns True if colorblind mode requested by the user. False otherwise.
+ */
 function isColorblindModeEnabled() {
   const colorblindCheck = document.getElementById("colorblindModeCheck");
   const isColorblindMode = colorblindCheck.checked;
@@ -80,6 +147,12 @@ function isColorblindModeEnabled() {
 }
 
 
+/**
+ * Determine if the user has requested sizing of glyphs by subpopulation size.
+ *
+ * @returns True if glpyhs should be sized according to subpopulation size or
+ *   false oterwise.
+ */
 function isSizingEnabled() {
   const sizingCheck = document.getElementById("groupSizeCheck");
   const isSizingMode = sizingCheck.checked;
@@ -87,16 +160,33 @@ function isSizingEnabled() {
 }
 
 
+/**
+ * Get the width of the display area for the website.
+ *
+ * @returns Width of the display area in pixels.
+ */
 function getClientWidth() {
   return document.documentElement.clientWidth;
 }
 
 
+/**
+ * Cache the current display area for the website.
+ *
+ * Cache the current display area for the website, will be used to determine if
+ * the display area has changed.
+ */
 function rememberClientWidth() {
   lastClientWidth = getClientWidth();
 }
 
 
+/**
+ * Get the list of groups to exclude (filter out) from dataset queries.
+ *
+ * @returns Array of strings for groups to exclude (names of groups like Male,
+ *   Female).
+ */
 function getRemovalList() {
   const filterChecks = Array.from(document.getElementsByClassName("filter-check"));
   const unchecked = filterChecks.filter((x) => !x.checked);
@@ -105,6 +195,11 @@ function getRemovalList() {
 }
 
 
+/**
+ * Clear the contents of the viz table and create a new presenter.
+ *
+ * @returns Newly created presenter.
+ */
 function createNewPresenter() {
   d3.select("#vizTableBody").html("");
   currentPresenter = new VizPresenter(MAX_PAY, MIN_GAP, MAX_GAP, MAX_GINI);
@@ -112,6 +207,12 @@ function createNewPresenter() {
 }
 
 
+/**
+ * Update the visualization.
+ *
+ * @param removalList List of groups (names of groups like Male, Female) to
+ *   exclude.
+ */
 function updateViz(removalList) {
   if (currentPresenter === null) {
     currentPresenter = createNewPresenter();
@@ -147,12 +248,21 @@ function updateViz(removalList) {
 }
 
 
+/**
+ * Clear existing elements and create a new presenter.
+ */
 function hardRedraw() {
   createNewPresenter();
   updateViz();
 }
 
 
+/**
+ * Callback for display area resize.
+ *
+ * On resize of window, create a new presenter and elements if the width of the
+ * display has chnged.
+ */
 function onResize() {
   if (lastClientWidth == getClientWidth()) {
     return;
@@ -163,16 +273,19 @@ function onResize() {
 }
 
 
+/**
+ * Register event listeners for the visualization and perform first render.
+ */
 function init() {
   initalLoadViz();
 
   rememberClientWidth();
   window.addEventListener("resize", onResize);
 
-  addCheckboxListenerId("zoomingAxisCheck");
-  addCheckboxListenerId("colorblindModeCheck");
+  addHardRedrawListenerId("zoomingAxisCheck");
+  addHardRedrawListenerId("colorblindModeCheck");
   const otherChecks = document.querySelectorAll(".filter-check");
-  otherChecks.forEach(addCheckboxListener);
+  otherChecks.forEach(addHardRedrawListener);
   
   addRedrawListener(document.getElementById("metric"));
   addRedrawListener(document.getElementById("groupSizeCheck"));
